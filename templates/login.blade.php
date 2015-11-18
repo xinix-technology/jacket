8b051f2d0c12b335be7d6edf7fbc0916e1c92fd2
Muhammad Ali <muhammadali.alaydrus@gmail.com>
2015-11-18 19:15:26 +0700

    [EDIT] Update login page

--- templates/login.blade.php
+++ templates/login.blade.php
@@ -4,26 +4,32 @@
    Login to System
 @endsection
 
-@section('customcss')
-   <link href="<?php echo Theme::base('vendor/naked2-theme/css/jacket.login.css') ?>" rel="stylesheet">
-@endsection
-
 @section('navbar')
 @endsection
 
 @section('sidebar')
 @endsection
 
-@section('body')
-   <div id="login">
-      @section('loginwording')
-         <i class="xn xn-lock xn-5x"></i>
-      @show
-      <form method="POST" class="row">
-         <input type="text" name="username" value="<?php echo @$entry['username'] ?>" placeholder="Username" />
-         <input type="password" name="password" placeholder="Password" />
-         <input type="submit" value="Login" class="button solid" />
-         <label class="placeholder"><input type="checkbox" class="checkbox"> Keep me sign in</label>
-      </form>
-   </div>
+@section('skeleton')
+	<div class="row">
+		<div class="xlarge-4 large-4 medium-3 small-1 tiny-1">
+			&nbsp;
+		</div>
+		<div class="xlarge-4 large-4 medium-6 small-10 tiny-10">
+			<div id="login">
+				@section('loginwording')
+					<i class="xn xn-lock-open xn-5x"></i>
+				@show
+				<form method="POST" class="row">
+					<input type="text" name="username" value="<?php echo @$entry['username'] ?>" placeholder="Username" />
+					<input type="password" name="password" placeholder="Password" />
+					<input type="submit" value="Login" class="button solid"/>
+					<label class="placeholder"><input type="checkbox" class="checkbox"> Keep me sign in</label>
+				</form>
+			</div>
+		</div>
+		<div class="xlarge-4 large-4 medium-3 small-1 tiny-1">
+			&nbsp;
+		</div>
+	</div>
 @endsection
